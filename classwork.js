f8f2128aa51ecb4b360f04c58bfeb44603c798f5
makedonecAlex <razario.aggro@gmail.com>
2022-06-16 19:18:07 +0300

    add homework file js

---
+++ classwork.js
@@ -0,0 +1,13 @@
+let apples = 15
+let cherry = 5
+
+let compere = apples >= cherry
+console.log("compere =", compere)
+
+if (compere){
+    console.log("Compere is", compere)
+} else if (apples == 5) {
+        console.log("apples ==", apples)
+} else {
+        console.log("!!_Compare is", false)
+}
