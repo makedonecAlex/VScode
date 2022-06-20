d836d5db7d6d41a5a493b09c894c32a52614352e
makedonecAlex <razario.aggro@gmail.com>
2022-06-20 20:57:50 +0300

    fix hw file

---
+++ classwork-2.js
@@ -0,0 +1,49 @@
+// console.log("hello")
+// console.log(true||false||false)
+// console.log(true&&false||true&&true)
+
+// if (false||true||false){
+//     console.log('1')
+// }else{
+//     console.log('2')
+// }
+
+// let dog_show = true
+// let age = 17
+// let money = 1000
+// let dog = true
+
+// if (age >= 18 && money >= 1000 && dog){
+//     console.log('Get ticket')
+// }else if (age < 18){
+//     console.log('Early')
+// }else if (money < 1000){
+//     console.log('Keep calm and work hard!!')
+// }else if (!dog){
+//     console.log('Get a cat')
+// }
+
+
+// switch (dog_show){
+//     case age >= 18;
+//         console.log('Time to win')
+//         break;
+//     case money >=1000
+// }
+
+// count = 0
+// while(count < 10){
+//     // console.log(count, 'Win')
+//     if (count % 2 == 0){
+//         console.log(count, 'even')
+//     }
+//         count++
+// }
+    
+
+mm = ['Alla', 'Natalia', 'Anatiliy', 'Dmitriy', 'Milana']
+// console.log(mm.length)
+
+for (let i = 0; i < mm.length + 1; i++){
+    console.log(i, mm[i], 'Win!!!')
+}
